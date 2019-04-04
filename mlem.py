71b5ebce489ed0e1c15212e8f56d5989483098b1
Phillip Bosek <raddcircles@gmail.com>
2019-04-04 18:56:48 -0400

    Bot Outline

--- mlem.py
+++ mlem.py
@@ -1,2 +1,12 @@
 import discord
 import configparser
+conf = configparser.ConfigParser()
+conf.read('./config.ini')
+bot = commands.Bot(command_prefix='mlem&')
+@bot.event
+async def on_ready():
+    print('Logged in as')
+    print(bot.user.name)
+    print(bot.user.id)
+    
+bot.run(conf.get('bot', 'token'))
